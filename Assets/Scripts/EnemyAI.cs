f4be572765845b652426e8125a0ed3b2ec7eef35
DaePrea <lccooper04@gmail.com>
2024-01-17 09:34:36 +0000

    Basic enemy AI and  player weapon
    added in some enemy ai as well as giving the player a weapon.
    
    Enemy can now chase you if you are within range of them. Cant do anything else at the moment.
    
    Player weapon can shoot but doesn't fire a projectile just the raycast. Gave the weapon a reticule so you know where you are shooting

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] float chaseRange = 5f;
+    float distanceToTarget;
+    float turnSpeed = 5f;
+    
+    [SerializeField] Transform playerTarget;
+    NavMeshAgent nMA;
+
+    bool isAggro = false;
+
+    void Start()
+    {
+        nMA = GetComponent<NavMeshAgent>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(isAggro)
+        {
+            EngageTarget();
+        }
+
+        else if(distanceToTarget <= chaseRange)
+        {
+            isAggro = true;
+        }
+
+
+        distanceToTarget = Vector3.Distance(playerTarget.position, transform.position);
+
+        if (distanceToTarget <= chaseRange)
+        {
+            nMA.SetDestination(playerTarget.position);
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= nMA.stoppingDistance)
+        {
+            nMA.SetDestination(playerTarget.position);
+        }
+
+        if(distanceToTarget <= nMA.stoppingDistance)
+        {
+            print("attacking");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (playerTarget.position - transform.position).normalized;
+        Quaternion angleToTurn = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, angleToTurn, Time.deltaTime * turnSpeed);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] float raycastRange = 100f;
+    
+    [SerializeField] Camera playerCam;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, raycastRange);
+        print(hit.transform.name);
+    }
+}
