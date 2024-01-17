1af22cecb43ee20b86a301424c43dde5056246de
DaePrea <lccooper04@gmail.com>
2024-01-17 04:01:08 +0000

    Added in player movement
    jumping currently not working though, will fix soon

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform cameraPosition;
+
+
+    void Update()
+    {
+        transform.position = cameraPosition.position;    
+    }
+}
