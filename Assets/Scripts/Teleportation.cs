8eb8cd50065a80bc2a784851e7e40867ecc2f50c
DaePrea <lccooper04@gmail.com>
2024-01-18 04:02:33 +0000

    teleporters
    Added in teleporters

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject TeleportHere;
+    
+
+    private void OnTriggerEnter(Collider collision)
+    {
+       
+            Player.transform.position = TeleportHere.transform.position;
+        
+    }
+}
