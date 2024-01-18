a83986413490a1b0ab69215045045d910c69b99c
DaePrea <lccooper04@gmail.com>
2024-01-18 11:44:14 +0000

    Player death
    added in a way for the player to die

---
+++ Assets/Scripts/Player scripts/PlayerDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Death"))
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
